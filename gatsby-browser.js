fcde0069f653f30772c5eb9f9264bad688f943ab
Lucas <lucashuguet31@gmail.com>
2020-01-11 21:43:59 +0100

    Add Services Workers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,13 @@
 
 // You can delete this file if you're not using it
 
-import "typeface-fira-sans"
+
+export const onServiceWorkerUpdateReady = () => {
+    const answer = window.confirm(
+      `This application has been updated. ` +
+        `Reload to display the latest version?`
+    )
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
